a877ef446fc669cb567c4aee89af2eba273b74ca
AndrewRoddy <andrewroddy7@gmail.com>
2023-09-29 21:49:31 -0400

    v0.20.00 Connect 4  running with 2D arrays

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # All code written by Andrew Roddy
 
-def AsciiArt():
-    return "\n   ____                            _     _  _\n  / ___|___  _ __  _ __   ___  ___| |_  | || |\n | |   / _ \| '_ \| '_ \ / _ \/ __| __| | || |_\n | |__| (_) | | | | | | |  __/ (__| |_  |__   _|\n  \____\___/|_| |_|_| |_|\___|\___|\__|    |_|\n\n\n"
+
+def ascii_art():
+    """Generates Ascii Art for the program
+
+    Returns:
+        str: ASCII Art
+    """
+    return "\n   ____                            _     _  _\n  / ___|___  _ __  _ __   ___  ___| |_  | || |\n | |   / _ \| '_ \| '_ \ / _ \/ __| __| | || |_\n | |__| (_) | | | | | | |  __/ (__| |_  |__   _|\n  \____\___/|_| |_|_| |_|\___|\___|\__|    |_|\nUse -1 to forfeit."
 
 
+def empty_board():
+    """Generates an empty Connect 4 board.
 
-def Empty_Board():
+    Returns:
+        list: A 2d list of the empty Connect 4 board.
+    """
     return [
         ["_", "_", "_", "_", "_", "_", "_"],
         ["_", "_", "_", "_", "_", "_", "_"],
@@ -16,185 +26,225 @@ def Empty_Board():
     ]
 
 
-def DisplayBoard(board):
+def display_board(board: list) -> None:
+    """Prints the board with column numbers to the terminal.
+
+    Args:
+        board (list): Prints the board to the terminal.
+    """
     print("1 2 3 4 5 6 7")
     for i in range(len(board)):
         print(*board[i], sep=" ")
-        
+
     print("1 2 3 4 5 6 7")
 
 
-def playerTurn(board, column):
-    for i in range(len(board[column])):
+def player_turn(board: list, column: int, player: str) -> list:
+    """Allows the player to place an icon on the board.
+
+    Args:
+        board (list): The current Connect 4 board.
+        column (int): The column that the user selected.
+        player (str): The current player's icon.
+
+    Returns:
+        list: The updated Connect 4 board.
+    """
+    column -= 1
+
+    for i in range(len(board) - 1, -1, -1):
         if board[i][column] == "_":
-            board[i][column] = "X"
-    
-    return board
-     
+            board[i][column] = player
+            return board
 
 
-def checkWinVert():
-    global GameRunning, Winner
-    check, check2, check3, check4, finder = -1, 6, 13, 20, False
-    while finder == False:
-        check += 1
-        check2 += 1
-        check3 += 1
-        check4 += 1
-        if (
-            board[check] == player
-            and board[check2] == player
-            and board[check3] == player
-            and board[check4] == player
-        ):
-            finder, GameRunning, Winner = True, False, player
-        if check4 == 41:
-            finder = True
+def check_turn(board: list, column: int) -> bool:
+    """Checks if the player's turn is valid.
+
+    Args:
+        board (list): The current Connect 4 board.
+        column (int): The column that the user selected.
+
+    Returns:
+        bool: True if the turn is valid, False if the turn is invalid.
+    """
+    if column > 7 or column < 0:
+        return False
+
+    if board[0][column - 1] != "_":
+        return False
+
+    return True
 
 
-def checkWinHorizontal():
-    global GameRunning, Winner
-    check, check2, check3, check4, finder = -1, 0, 1, 2, False
-    while finder == False:
-        check += 1
-        check2 += 1
-        check3 += 1
-        check4 += 1
-        if (
-            board[check] == player
-            and board[check2] == player
-            and board[check3] == player
-            and board[check4] == player
-        ):
-            finder, GameRunning, Winner = True, False, player
-        if check4 == 6 or check4 == 13 or check4 == 20 or check4 == 27 or check4 == 34:
-            check += 3
-            check2 += 3
-            check3 += 3
-            check4 += 3
-        if check4 == 41:
-            finder = True
+def check_win_horizontal(board: list, player: str) -> bool:
+    """Checks if the current user has won the game horizontally.
 
+    Args:
+        board (list): The current Connect 4 board.
+        player (str): The current player's icon.
 
-def checkWinDiagnol():
-    global GameRunning, Winner
-    check, check2, check3, check4, finder = 20, 14, 8, 2, False
-    while finder == False:
-        check += 1
-        check2 += 1
-        check3 += 1
-        check4 += 1
-        if (
-            board[check] == player
-            and board[check2] == player
-            and board[check3] == player
-            and board[check4] == player
-        ):
-            finder, GameRunning, Winner = True, False, player
-        if check4 == 6 or check4 == 13:
-            check += 3
-            check2 += 3
-            check3 += 3
-            check4 += 3
-        if check4 == 20:
-            finder = True
-
-
-def checkWinBackDiag():
-    global GameRunning, Winner
-    check, check2, check3, check4, finder = -1, 7, 15, 23, False
-    while finder == False:
-        check += 1
-        check2 += 1
-        check3 += 1
-        check4 += 1
-        if (
-            board[check] == player
-            and board[check2] == player
-            and board[check3] == player
-            and board[check4] == player
-        ):
-            finder, GameRunning, Winner = True, False, player
-        if check4 == 27 or check4 == 34:
-            check += 3
-            check2 += 3
-            check3 += 3
-            check4 += 3
-        if check4 == 41:
-            finder = True
-
-
-def checkWin():
-    global player
-    checkWinVert()
-    checkWinHorizontal()
-    checkWinDiagnol()
-    checkWinBackDiag()
-
-
-def RunGame():
-    global player, turnNumber, GameRunning
-    DisplayBoard()
-    while GameRunning == True:
-        player, turnNumber = "M", "000"
-        turnNumber = input("M's Turn:  ")
-        while (
-            turnNumber != "1"
-            and turnNumber != "2"
-            and turnNumber != "3"
-            and turnNumber != "4"
-            and turnNumber != "5"
-            and turnNumber != "6"
-            and turnNumber != "7"
-        ):
-            turnNumber = input("M's Turn:  ")
-        playerTurn()
-        DisplayBoard()
-        checkWin()
-        if GameRunning == True:
-            player, turnNumber = "O", "000"
-            turnNumber = input("O's Turn:  ")
-            while (
-                turnNumber != "1"
-                and turnNumber != "2"
-                and turnNumber != "3"
-                and turnNumber != "4"
-                and turnNumber != "5"
-                and turnNumber != "6"
-                and turnNumber != "7"
+    Returns:
+        bool: True if the player has won, False if the player has not won.
+    """
+    for x in range(len(board[0]) - 3):
+        for y in range(len(board)):
+            if (
+                board[y][x] == player
+                and board[y][x + 1] == player
+                and board[y][x + 2] == player
+                and board[y][x + 3] == player
             ):
-                turnNumber = input("O's Turn:  ")
-            playerTurn()
-            DisplayBoard()
-            checkWin()
-    print(f"{Winner} Wins!!!")
+                return True
+    return False
 
- 
+
+def check_win_vertical(board: list, player: str) -> bool:
+    """Checks if the current user has won the game vertically.
+
+    Args:
+        board (list): The current Connect 4 board.
+        player (str): The current player's icon.
+
+    Returns:
+        bool: True if the player has won, False if the player has not won.
+    """
+    for y in range(len(board[0]) - 4):
+        for x in range(len(board)):
+            if (
+                board[y][x] == player
+                and board[y + 1][x] == player
+                and board[y + 2][x] == player
+                and board[y + 3][x] == player
+            ):
+                return True
+    return False
+
+
+def check_win_diagnol(board: list, player: str) -> bool:
+    """Checks if the current user has won the game diagnoly.
+
+    Args:
+        board (list): The current Connect 4 board.
+        player (str): The current player's icon.
+
+    Returns:
+        bool: True if the player has won, False if the player has not won.
+    """
+    for y in range(len(board[0]) - 4):
+        for x in range(len(board) - 2):
+            if (
+                board[y + 3][x] == player
+                and board[y + 2][x + 1] == player
+                and board[y + 1][x + 2] == player
+                and board[y][x + 3] == player
+            ):
+                return True
+    return False
+
+
+def check_win_reverse_diagnol(board: list, player: str) -> bool:
+    """Checks if the current user has won the game in a reverse diagnol.
+
+    Args:
+        board (list): The current Connect 4 board.
+        player (str): The current player's icon.
+
+    Returns:
+        bool: True if the player has won, False if the player has not won.
+    """
+    for y in range(len(board[0]) - 4):
+        for x in range(len(board) - 2):
+            if (
+                board[y][x] == player
+                and board[y + 1][x + 1] == player
+                and board[y + 2][x + 2] == player
+                and board[y + 3][x + 3] == player
+            ):
+                return True
+    return False
+
+
+def check_win(board: list, player: str) -> bool:
+    """Runs the 4 win checking functions and returns True if any of them return True.
+
+    Args:
+        board (list): The current Connect 4 board.
+        player (str): The current player's icon.
+
+    Returns:
+        bool: True if the player has won.
+    """
+    return (
+        check_win_horizontal(board, player)
+        or check_win_vertical(board, player)
+        or check_win_reverse_diagnol(board, player)
+        or check_win_diagnol(board, player)
+    )
+
+
+def check_full(board: list) -> bool:
+    """Checks if the players have filled the board and tied.
+
+    Args:
+        board (list): The current Connect 4 board.
+
+    Returns:
+        bool: True if the board is full, False if the board is not full.
+    """
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == "_":
+                return False
+    return True
+
+
 def main():
-    board = Empty_Board()
+    print(ascii_art())
+    board = empty_board()
+    player = "X"
     while True:
-        DisplayBoard(board)
-        column = int(input("Where do u wanna go?"))
-        board = playerTurn(board, column)
-    """
-        global GameRunning
+        print("\n")
+        display_board(board)
+        try:
+            column = int(input(f"What column {player}? "))
+            if column == -1:
+                break
 
-        GameRunning, Winner, playAgain = True, "Nobody", "yes"
+        except ValueError:
+            print("Enter a number")
+            continue
 
-        print(AsciiArt())
-        while (
-        playAgain.lower() == "yes"
-        or playAgain.lower() == "y"
-        or playAgain.lower() == "si"
-        or playAgain.lower() == "play"
-        or playAgain == "1"
-        ):
-            GameRunning = True
-            Variables()
-            RunGame()
-            playAgain = input("Would you like to play again?\n")
-            print("Thanks for playing!!")
-    """
-    
+        if check_turn(board, column) == False:
+            print("Column is full or out of range")
+            continue
+
+        column = int(column)
+
+        board = player_turn(board, column, player)
+
+        if check_win(board, player):
+            display_board(board)
+            print(f"{player} Wins!\n")
+            break
+
+        if check_full(board):
+            display_board(board)
+            print("Tie!")
+            break
+        
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+            
+
+    print("Thanks for playing!")
+    rematch = input("Play Again? ")
+
+    if rematch.lower() == "y" or rematch.lower() == "yes" or rematch.lower() == "1":
+        main()
+
+
 if __name__ == "__main__":
     main()
